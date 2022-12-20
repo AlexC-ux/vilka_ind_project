78a58914336309f0de45f1ef3b706fd1e6257c54
AlexC-ux <eunv2004@gmail.com>
2022-12-20 08:56:23 +0300

    animjs evang

--- pages/evangelion.tsx
+++ pages/evangelion.tsx
@@ -1,4 +1,58 @@
-export default function EvangelionPage(){
-    return(<>
+import Evangelion_svg from "../SVGR/evangelion_svg";
+import EvangelionLogo_svg from "../SVGR/evangelion_logo_svg";
+import animejs from "animejs";
+import { useEffect } from "react";
+import { Box } from "@mui/material";
+
+export default function EvangelionPage() {
+
+    useEffect(() => {
+        animejs({
+            targets: ["#__next > div > div > div:nth-child(1) > svg > path"],
+            strokeDashoffset: [animejs.setDashoffset, 0],
+            easing: 'easeInOutSine',
+            duration: 10000,
+            delay: function (el, i) { return i * 750 },
+            direction: 'normal',
+        })
+        animejs({
+            targets: ["#__next > div > div > div:nth-child(2) > svg > path"],
+            strokeDashoffset: [animejs.setDashoffset, 0],
+            easing: 'cubicBezier(0.92,0.08,0.71,0.87);',
+            duration: 2500,
+            delay: function (el, i) { return i * 20 },
+            direction: 'normal',
+        })
+    })
+
+    return (<>
+        <Box>
+            <Box
+                className="evangelionTextBox"
+                sx={{
+                    svg: {
+                        mx: "auto",
+                        display: "block",
+                        maxHeight: "10rem",
+                        mt: 4,
+                    }
+                }}>
+                <Evangelion_svg />
+            </Box>
+
+            <Box
+                className="evangelionTextBox"
+                sx={{
+                    svg: {
+                        mx: "auto",
+                        display: "block",
+                        maxHeight: "15rem",
+                        height:"40vw",
+                        mt: 4,
+                    }
+                }}>
+                <EvangelionLogo_svg/>
+            </Box>
+        </Box>
     </>)
 }
